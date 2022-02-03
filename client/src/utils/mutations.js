259e72268d891acf9157c27757500e53b01556d2
Victor Weinert <dev@vweinert.com>
2022-02-02 18:28:18 -0600

    added login & create user functionality on front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const loginUser = gql`
+export const LOGIN_USER = gql`
   mutation loginUser($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
@@ -12,7 +12,7 @@ export const loginUser = gql`
   }
 `;
 
-export const addUser = gql`
+export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
@@ -25,7 +25,7 @@ export const addUser = gql`
   }
 `;
 
-export const saveBook = gql`
+export const SAVE_BOOK = gql`
   mutation saveBook ($input: SavedBookInput) {
     saveBook (input: $input) {
       _id
@@ -43,7 +43,7 @@ export const saveBook = gql`
   }
 `;
 
-export const removeBook = gql`
+export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
       _id
